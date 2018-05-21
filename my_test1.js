1a39853afed4144b00c55da71a49e2e4d90d92a5
pohua920 <pohua920@gmail.com>
2018-05-21 15:39:04 +0800

    弄個衝突出來玩

--- my_test1.js
+++ my_test1.js
@@ -2,6 +2,16 @@
 
 彩虹-紅色 橙色 黃色 綠色 藍色 靛色 紫色
 remote no.180514_001
+local no.180514007
+local no.180514_008
+local no.180514_009
 remote no.180514_005
 local no.180514004
-remote no.180514_003
+local no.180514_003
+local no.180514_008
+local no.180514_009
+本機端搞個衝突
+local no.180514_010
+local no.180514_012
+
+local no.180514_011
